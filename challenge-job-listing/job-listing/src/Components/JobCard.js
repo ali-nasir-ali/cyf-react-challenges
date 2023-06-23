3efdc0020c2f7ef6b174518cd9001fa2a7dc5c53
nasir ali <a.nasirali@outlook.com>
2023-06-23 16:47:02 +0100

    moved images to public from src and they work now

--- challenge-job-listing/job-listing/src/Components/JobCard.js
+++ challenge-job-listing/job-listing/src/Components/JobCard.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 function JobCard({ job }) {
+  const publicURL = process.env.PUBLIC_URL;
+  //   console.log({job.logo});
   return (
     <div className="job-card">
-      <img src={job.logo} alt={`${job.company} logo`} />
+      <img src={`${publicURL}${job.logo}`} alt={`${job.company} logo`} />
       <div className="job-info">
         <div className="job-header">
           <h3>{job.company}</h3>
