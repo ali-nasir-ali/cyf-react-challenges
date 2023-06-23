3f2f85c4d5049cb61a207b487a0de25b97b75cb9
nasir ali <a.nasirali@outlook.com>
2023-06-23 15:00:51 +0100

    working on the job listing react app

---
+++ challenge-job-listing/job-listing/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import JobList from "./Components/JobList";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <JobList />
+    </div>
+  );
+}
+
+export default App;
---
+++ challenge-job-listing/job-listing/src/Components/JobCard.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function JobCard({ job }) {
+  return (
+    <div className="job-card">
+      <img src={job.logo} alt={`${job.company} logo`} />
+      <div className="job-info">
+        <div className="job-header">
+          <h3>{job.company}</h3>
+          {job.new && <span className="job-badge new">New!</span>}
+          {job.featured && <span className="job-badge featured">Featured</span>}
+        </div>
+        <h2>{job.position}</h2>
+        <div className="job-details">
+          <span>{job.postedAt}</span>
+          <span>{job.contract}</span>
+          <span>{job.location}</span>
+        </div>
+      </div>
+      <hr />
+      <div className="job-requirements">
+        {job.languages.map((language) => (
+          <span key={language}>{language}</span>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default JobCard;
